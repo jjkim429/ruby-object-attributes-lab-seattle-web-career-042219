6ebb2902274ef6e822e05209b9dbf57d6d5091fb
Jennifer Kim <jjkim429@gmail.com>
2019-04-23 05:51:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
   
+  def name
+    @name
+  end
+  
   def name=(name)
     @name = name
   end
   
-  def name
-    @name = name
+  def breed
+    @breed
   end
   
   def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
     @breed = breed
   end
   
--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,19 @@
 class Person
   
+  def name
+    @name
+  end
+  
   def name=(name)
     @name = name
   end
-  
-  def name
-    @name = name
+ 
+  def job
+    @job
   end
   
   def job=(job)
     @job = job
   end
-  
-  def job
-    @job = job
-  
+
 end
